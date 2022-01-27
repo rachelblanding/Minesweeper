7a7d9ad6a779cec69c03b8320c1af7e28f686100
rachelblanding <66678381+rachelblanding@users.noreply.github.com>
2022-01-26 19:35:15 -0500

    Add files via upload
    original classes and images of the minesweeper game

---
+++ main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class main {
+	public static void main(String[] args) {
+		board myboard = new board();
+		myboard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myboard.setVisible(true);
+	}
+
+}
